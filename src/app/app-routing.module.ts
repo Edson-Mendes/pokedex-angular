1fbb134c33aad626a809829e1dc16b30357f5ecc
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-11-02 10:27:20 -0300

    feat: add card component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonsComponent } from './page/pokemons/pokemons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pokemons',
+    component: PokemonsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'pokemons',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonsComponent } from './page/pokemons/pokemons.component';
+import { CardComponent } from './component/card/card.component';
+import { TypeListComponent } from './component/type-list/type-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonsComponent,
+    CardComponent,
+    TypeListComponent
   ],
   imports: [
     BrowserModule,
